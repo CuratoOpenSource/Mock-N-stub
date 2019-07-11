f2dcac8d8d226161cc4b1e843040c27922c6dc19
Menno Lovink <mclovink@me.com>
2019-07-11 10:32:53 +0200

    be more strict about dependency versions

--- Example/Pods/Nimble/Sources/Nimble/Matchers/BeEmpty.swift
+++ Example/Pods/Nimble/Sources/Nimble/Matchers/BeEmpty.swift
@@ -4,10 +4,11 @@ import Foundation
 /// means the are no items in that collection. For strings, it is an empty string.
 public func beEmpty<S: Sequence>() -> Predicate<S> {
     return Predicate.simple("be empty") { actualExpression in
-        guard let actual = try actualExpression.evaluate() else {
+        let actualSeq = try actualExpression.evaluate()
+        if actualSeq == nil {
             return .fail
         }
-        var generator = actual.makeIterator()
+        var generator = actualSeq!.makeIterator()
         return PredicateStatus(bool: generator.next() == nil)
     }
 }
@@ -15,17 +16,6 @@ public func beEmpty<S: Sequence>() -> Predicate<S> {
 /// A Nimble matcher that succeeds when a value is "empty". For collections, this
 /// means the are no items in that collection. For strings, it is an empty string.
 public func beEmpty<S: SetAlgebra>() -> Predicate<S> {
-    return Predicate.simple("be empty") { actualExpression in
-        guard let actual = try actualExpression.evaluate() else {
-            return .fail
-        }
-        return PredicateStatus(bool: actual.isEmpty)
-    }
-}
-
-/// A Nimble matcher that succeeds when a value is "empty". For collections, this
-/// means the are no items in that collection. For strings, it is an empty string.
-public func beEmpty<S: Sequence & SetAlgebra>() -> Predicate<S> {
     return Predicate.simple("be empty") { actualExpression in
         guard let actual = try actualExpression.evaluate() else {
             return .fail
