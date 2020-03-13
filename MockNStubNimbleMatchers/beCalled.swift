f9bfedc720e53d8fcce662d68da98ca5dddfaa43
Menno Lovink <mclovink@me.com>
2020-03-13 12:23:28 +0100

    boyscout - rearange file

--- MockNStubNimbleMatchers/beCalled.swift
+++ MockNStubNimbleMatchers/beCalled.swift
@@ -5,10 +5,40 @@
 import Nimble
 import MockNStub
 
-public func beCalled<T: Mocking & DefiningFunctionID, ARGS>(_ amount: Verification.Amount,
-                                                      timesAt functionID: T.FunctionID,
-                                                      withArgumentsThatMatch matcher: ((ARGS)->Bool)?) -> Predicate<T> {
 
+//MARK: - Public
+
+//MARK: Without argument matching
+
+public func beCalledOnce<T: Mocking & DefiningFunctionID>(at functionID: T.FunctionID) -> Predicate<T> {
+    return beCalled(.exactly(amount: 1), timesAt: functionID, withArgumentsThatMatch: matcherThatAlwaysPasses)
+}
+
+public func beCalled<T: Mocking & DefiningFunctionID>(at functionID: T.FunctionID) -> Predicate<T> {
+    return beCalled(.anyAmount, timesAt: functionID, withArgumentsThatMatch: matcherThatAlwaysPasses)
+}
+
+public func beCalled<T: Mocking & DefiningFunctionID>(_ amount: Verification.Amount,
+                                                      timesAt functionID: T.FunctionID) -> Predicate<T> {
+    return beCalled(amount, timesAt: functionID, withArgumentsThatMatch: matcherThatAlwaysPasses)
+}
+
+//MARK: With argument matching
+
+public func beCalledOnce<T: Mocking & DefiningFunctionID, ARGS>(at functionID: T.FunctionID,
+                                                                withArgumentsThatMatch matcher: @escaping (ARGS)->Bool) -> Predicate<T> {
+    return beCalled(.exactly(amount: 1), timesAt: functionID, withArgumentsThatMatch: matcher)
+}
+
+public func beCalled<T: Mocking & DefiningFunctionID, ARGS>(at functionID: T.FunctionID,
+                                                            withArgumentsThatMatch matcher: @escaping (ARGS)->Bool) -> Predicate<T> {
+    return beCalled(.anyAmount, timesAt: functionID, withArgumentsThatMatch: matcher)
+}
+
+public func beCalled<T: Mocking & DefiningFunctionID, ARGS>(_ amount: Verification.Amount,
+                                                            timesAt functionID: T.FunctionID,
+                                                            withArgumentsThatMatch matcher: ((ARGS)->Bool)?) -> Predicate<T> {
+    
     return Predicate { (actualExpression: Expression<T>) throws -> PredicateResult in
         let expectationMessage = "be called \(amount) times at \(functionID.rawValue)\(matcher != nil ? " based on passed argumentMatcher" : "")"
         
@@ -33,29 +63,8 @@ public func beCalled<T: Mocking & DefiningFunctionID, ARGS>(_ amount: Verificati
     }
 }
 
+//MARK: - Private
+
 private let matcherThatAlwaysPasses = { (args: Any?) -> Bool in
     return true
-}
-
-public func beCalledOnce<T: Mocking & DefiningFunctionID>(at functionID: T.FunctionID) -> Predicate<T> {
-    return beCalled(.exactly(amount: 1), timesAt: functionID, withArgumentsThatMatch: matcherThatAlwaysPasses)
-}
-
-public func beCalledOnce<T: Mocking & DefiningFunctionID, ARGS>(at functionID: T.FunctionID,
-                                                                withArgumentsThatMatch matcher: @escaping (ARGS)->Bool) -> Predicate<T> {
-    return beCalled(.exactly(amount: 1), timesAt: functionID, withArgumentsThatMatch: matcher)
-}
-
-public func beCalled<T: Mocking & DefiningFunctionID>(at functionID: T.FunctionID) -> Predicate<T> {
-    return beCalled(.anyAmount, timesAt: functionID, withArgumentsThatMatch: matcherThatAlwaysPasses)
-}
-
-public func beCalled<T: Mocking & DefiningFunctionID, ARGS>(at functionID: T.FunctionID,
-                                                            withArgumentsThatMatch matcher: @escaping (ARGS)->Bool) -> Predicate<T> {
-    return beCalled(.anyAmount, timesAt: functionID, withArgumentsThatMatch: matcher)
-}
-
-public func beCalled<T: Mocking & DefiningFunctionID>(_ amount: Verification.Amount,
-                                                      timesAt functionID: T.FunctionID) -> Predicate<T> {
-    return beCalled(amount, timesAt: functionID, withArgumentsThatMatch: matcherThatAlwaysPasses)
 }
